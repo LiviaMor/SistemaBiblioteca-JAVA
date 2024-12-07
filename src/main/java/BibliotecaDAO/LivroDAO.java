7fbf2981e1d2bd0f403e62e5f03aa705c33c054c
ScarletMor <liviamor01@hotmail.com>
2024-12-07 19:33:20 -0300

    Update LivroDAO.java

--- src/main/java/BibliotecaDAO/LivroDAO.java
+++ src/main/java/BibliotecaDAO/LivroDAO.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LivroDAO {
+
+	// Create
 	public void adicionarLivro(Livro livro) {
 		String sql = "INSERT INTO livro (codigo, titulo, autor, ano) VALUES (?, ?, ?, ?)";
 		try (Connection conn = ConexaoBD.conectar(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -20,6 +22,7 @@ public class LivroDAO {
 		}
 	}
 
+	// Read
 	public List<Livro> buscarTodosLivros() {
 		String sql = "SELECT * FROM livro";
 		List<Livro> livros = new ArrayList<>();
@@ -37,4 +40,31 @@ public class LivroDAO {
 		return livros;
 	}
 
+	// Update
+	public void atualizarLivro(Livro livro) {
+		String sql = "UPDATE livro SET codigo = ?, titulo = ?, autor = ?, ano = ? WHERE id_livro = ?";
+		try (Connection conn = ConexaoBD.conectar(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, livro.getCodigo());
+			pstmt.setString(2, livro.getTitulo());
+			pstmt.setString(3, livro.getAutor());
+			pstmt.setString(4, livro.getAno());
+			pstmt.setInt(5, livro.getId_livro());
+			pstmt.executeUpdate();
+			System.out.println("Livro atualizado com sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Delete
+	public void deletarLivro(int id_livro) {
+		String sql = "DELETE FROM livro WHERE codigo = ?";
+		try (Connection conn = ConexaoBD.conectar(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, id_livro);
+			pstmt.executeUpdate();
+			System.out.println("Livro deletado com sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
